ae2f60e342b57b9c9c21d49bbd56e0f82b437eaf
UnderTheMoonspell <bernardoazevedo88@gmail.com>
2016-07-15 18:07:21 +0100

    Webpack like a boss!

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import ButtonView from './AppPart2';
-import App3 from './AppPart3';
-import App4 from './AppPart4';
+import App from './src/App';
+import ButtonView from './src/AppPart2';
+import App3 from './src/AppPart3';
+import App4 from './src/AppPart4';
 // ReactDOM.render(<ButtonView />, document.getElementById('app'))
 // ReactDOM.render(<App3 />, document.getElementById('app'))
+console.log("env: " + process.env.NODE_ENV);
 ReactDOM.render(<App4 />, document.getElementById('app'))
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,12 @@
-module.exports = {
+var webpack = require('webpack'),
+    production = process.env.NODE_ENV === 'production',
+    path = require('path');
+
+
+options = {
     entry: './main.js',
     output: {
-        path:'./',
+        path: path.join(__dirname, 'dist'),
         filename: 'index.js'
     },
     devServer: {
@@ -17,7 +22,36 @@ module.exports = {
                 query: {
                     presets: ['es2015', 'react']
                 }
-            }
+            },
+            {
+                test:   /\.(png|gif|jpe?g|svg)$/i,
+                loader: 'url?limit=10000',
+            },   
+            {
+            test: /\.css$/,
+            loader: "css-loader!autoprefixer-loader"
+            },
+            {
+            test: /\.scss$/,
+            loader: "css-loader!sass-loader"
+            }                             
         ]
-    }
+    },
+    plugins: [
+    ]
 }
+
+if (production){
+
+    options.plugins = options.plugins.concat([
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                drop_console: true,
+                screw_ie8: true,
+                warnings: false
+            }
+        })
+    ]);  
+}
+
+module.exports = options;
